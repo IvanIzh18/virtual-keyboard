75841976cfe4f53555da6e28d58a3e0fe28e6f7f
IvanIzh18 <96039558+IvanIzh18@users.noreply.github.com>
2022-05-10 00:22:03 +0400

    feat: added scripts

---
+++ script.js
@@ -0,0 +1,560 @@
+const symbol = `[{
+    "eventCode": "Backquote",
+    "eng": ["&#96;", "&#126;"],
+    "rus": ["ё", "Ё"]
+}, {
+    "eventCode": "Digit1",
+    "eng": ["1", "&#33;"],
+    "rus": ["1", "&#33;"]
+}, {
+    "eventCode": "Digit2",
+    "eng": ["2", "&#64;"],
+    "rus": ["2", "&#34;"]
+}, {
+    "eventCode": "Digit3",
+    "eng": ["3", "&#35;"],
+    "rus": ["3", "&#8470;"]
+}, {
+    "eventCode": "Digit4",
+    "eng": ["4", "&#36;"],
+    "rus": ["4", "&#59;"]
+}, {
+    "eventCode": "Digit5",
+    "eng": ["5", "&#37;"],
+    "rus": ["5", "&#37;"]
+}, {
+    "eventCode": "Digit6",
+    "eng": ["6", "&#94;"],
+    "rus": ["6", "&#58;"]
+}, {
+    "eventCode": "Digit7",
+    "eng": ["7", "&#38;"],
+    "rus": ["7", "&#63;"]
+}, {
+    "eventCode": "Digit8",
+    "eng": ["8", "&#42;"],
+    "rus": ["8", "&#42;"]
+}, {
+    "eventCode": "Digit9",
+    "eng": ["9", "&#40;"],
+    "rus": ["9", "&#40;"]
+}, {
+    "eventCode": "Digit0",
+    "eng": ["0", "&#41;"],
+    "rus": ["0", "&#41;"]
+}, {
+    "eventCode": "Minus",
+    "eng": ["&#45;", "&#95;"],
+    "rus": ["&#45;", "&#95;"]
+}, {
+    "eventCode": "Equal",
+    "eng": ["&#61;", "&#43;"],
+    "rus": ["&#61;", "&#43;"]
+}, {
+    "eventCode": "Backspace",
+    "eng": ["Backspace", "Backspace"],
+    "rus": ["Backspace", "Backspace"]
+},
+{
+    "eventCode": "Tab",
+    "eng": ["Tab;", "Tab;"],
+    "rus": ["Tab;", "Tab;"]
+},
+{
+    "eventCode": "KeyQ",
+    "eng": ["q", "Q"],
+    "rus": ["й", "Й"]
+},
+{
+    "eventCode": "KeyW",
+    "eng": ["w", "W"],
+    "rus": ["ц", "Ц"]
+},
+{
+
+    "eventCode": "KeyE",
+    "eng": ["e", "E"],
+    "rus": ["у", "У"]
+},
+{
+    "eventCode": "KeyR",
+    "eng": ["r", "R"],
+    "rus": ["к", "К"]
+},
+{
+    "eventCode": "KeyT",
+    "eng": ["t", "T"],
+    "rus": ["е", "Е"]
+},
+{
+    "eventCode": "KeyY",
+    "eng": ["y", "Y"],
+    "rus": ["н", "Н"]
+},
+{
+    "eventCode": "KeyU",
+    "eng": ["u", "U"],
+    "rus": ["г", "Г"]
+},
+{
+    "eventCode": "KeyI",
+    "eng": ["i", "I"],
+    "rus": ["ш", "Ш"]
+},
+{
+    "eventCode": "KeyO",
+    "eng": ["o", "O"],
+    "rus": ["щ", "Щ"]
+},
+{
+    "eventCode": "KeyP",
+    "eng": ["p", "P"],
+    "rus": ["з", "З"]
+},
+{
+    "eventCode": "BracketLeft",
+    "eng": ["[", "{"],
+    "rus": ["х", "Х"]
+},
+{
+    "eventCode": "BracketRight",
+    "eng": ["]", "}"],
+    "rus": ["ъ", "Ъ"]
+},
+{
+    "eventCode": "Backslash",
+    "eng": ["&#92;", "&#124;"],
+    "rus": ["&#92;", "&#47;"]
+},
+{
+    "eventCode": "Delete",
+    "eng": ["Del", "Del"],
+    "rus": ["Del", "Del"]
+},
+{
+    "eventCode": "CapsLock",
+    "eng": ["CapsLock", "CapsLock"],
+    "rus": ["CapsLock", "CapsLock"]
+},
+{
+    "eventCode": "KeyA",
+    "eng": ["a", "A"],
+    "rus": ["ф", "Ф"]
+},
+{
+    "eventCode": "KeyS",
+    "eng": ["s", "S"],
+    "rus": ["ы", "Ы"]
+}, {
+    "eventCode": "KeyD",
+    "eng": ["d", "D"],
+    "rus": ["в", "В"]
+}, {
+    "eventCode": "KeyF",
+    "eng": ["f", "F"],
+    "rus": ["а", "А"]
+}, {
+    "eventCode": "KeyG",
+    "eng": ["g", "G"],
+    "rus": ["п", "П"]
+}, {
+    "eventCode": "KeyH",
+    "eng": ["h", "H"],
+    "rus": ["р", "Р"]
+}, {
+    "eventCode": "KeyJ",
+    "eng": ["j", "J"],
+    "rus": ["о", "О"]
+}, {
+    "eventCode": "KeyK",
+    "eng": ["k", "K"],
+    "rus": ["л", "Л"]
+}, {
+    "eventCode": "KeyL",
+    "eng": ["l", "L"],
+    "rus": ["д", "Д"]
+}, {
+    "eventCode": "Semicolon",
+    "eng": ["&#59;", "&#58;"],
+    "rus": ["ж", "Ж"]
+}, {
+    "eventCode": "Quote",
+    "eng": ["&#39;", "&#34;"],
+    "rus": ["э", "Э"]
+}, {
+    "eventCode": "Enter",
+    "eng": ["Enter", "Enter"],
+    "rus": ["Enter", "Enter"]
+}, {
+    "eventCode": "ShiftLeft",
+    "eng": ["Shift", "Shift"],
+    "rus": ["Shift", "Shift"]
+}, {
+    "eventCode": "KeyZ",
+    "eng": ["z", "Z"],
+    "rus": ["я", "Я"]
+}, {
+    "eventCode": "KeyX",
+    "eng": ["x", "X"],
+    "rus": ["ч", "Ч"]
+}, {
+    "eventCode": "KeyC",
+    "eng": ["c", "C"],
+    "rus": ["с", "С"]
+}, {
+    "eventCode": "KeyV",
+    "eng": ["v", "V"],
+    "rus": ["м", "М"]
+}, {
+    "eventCode": "KeyB",
+    "eng": ["b", "B"],
+    "rus": ["и", "И"]
+}, {
+    "eventCode": "KeyN",
+    "eng": ["n", "N"],
+    "rus": ["т", "Т"]
+}, {
+    "eventCode": "KeyM",
+    "eng": ["m", "M"],
+    "rus": ["ь", "Ь"]
+}, {
+    "eventCode": "Comma",
+    "eng": ["&#44;", "&#60;"],
+    "rus": ["б", "Б"]
+}, {
+    "eventCode": "Period",
+    "eng": ["&#46;", "&#62;"],
+    "rus": ["ю", "Ю"]
+}, {
+    "eventCode": "Slash",
+    "eng": ["&#47;", "&#63;"],
+    "rus": ["&#46;", "&#44;"]
+}, {
+    "eventCode": "ArrowUp",
+    "eng": ["&#8593;", "&#8593;"],
+    "rus": ["&#8593;", "&#8593;"]
+}, {
+    "eventCode": "ShiftRight",
+    "eng": ["Shift", "Shift"],
+    "rus": ["Shift", "Shift"]
+}, {
+    "eventCode": "ControlLeft",
+    "eng": ["Ctrl", "Ctrl"],
+    "rus": ["Ctrl", "Ctrl"]
+}, {
+    "eventCode": "MetaLeft",
+    "eng": ["Win", "Win"],
+    "rus": ["Win", "Win"]
+},  {
+    "eventCode": "AltLeft",
+    "eng": ["Alt", "Alt"],
+    "rus": ["Alt", "Alt"]
+}, {
+    "eventCode": "Space",
+    "eng": ["&nbsp;", "&nbsp;"],
+    "rus": ["&nbsp;", "&nbsp;"]
+}, {
+    "eventCode": "AltRight",
+    "eng": ["Alt", "Alt"],
+    "rus": ["Alt", "Alt"]
+}, {
+    "eventCode": "ArrowLeft",
+    "eng": ["&#8592;", "&#8592;"],
+    "rus": ["&#8592;", "&#8592;"]
+}, {
+    "eventCode": "ArrowDown",
+    "eng": ["&#8595;", "&#8595;"],
+    "rus": ["&#8595;", "&#8595;"]
+}, {
+    "eventCode": "ArrowRight",
+    "eng": ["&#8594;", "&#8594;"],
+    "rus": ["&#8594;", "&#8594;"]
+}, {
+    "eventCode": "ControlRight",
+    "eng": ["Ctrl", "Ctrl"],
+    "rus": ["Ctrl", "Ctrl"]
+}
+]`;
+
+// Прописываем саму страницу
+
+let symbolArray = JSON.parse(symbol);
+
+let titleKeyboard = document.createElement('div');
+titleKeyboard.className = "titleKeyboard";
+titleKeyboard.innerHTML = 'Виртуальная клавиатура'
+document.body.append(titleKeyboard);
+
+let fieldText = document.createElement('textarea');
+fieldText.innerHTML = '';
+fieldText.className = "fieldText";
+fieldText.setAttribute("rows", "10")
+document.body.append(fieldText);
+
+let bodyKeyboard = document.createElement('div');
+bodyKeyboard.innerHTML = "";
+bodyKeyboard.className = "bodyKeyboard";
+
+document.body.append(bodyKeyboard);
+const bodyKeyboardHTML = document.querySelector('.bodyKeyboard');
+
+for (let i = 0; i < symbolArray.length; i++) {
+    if (i == 0 || (symbolArray[i].eventCode == "Tab") || (symbolArray[i].eventCode == "CapsLock") || (symbolArray[i].eventCode == "ShiftLeft") || (symbolArray[i].eventCode == "ControlLeft")) {
+        let keyboardRow = document.createElement('div');
+        keyboardRow.innerHTML = ``;
+        keyboardRow.className = `keyboardRow`;
+        bodyKeyboardHTML.append(keyboardRow);
+    }
+
+    let keyboardKey = document.createElement('div');
+    keyboardKey.innerHTML = `
+        <span class="eng">
+            <span class="caseDown ">${symbolArray[i].eng[0]}</span>
+            <span class="caseUp hidden ">${symbolArray[i].eng[1]}</span>
+            <span class="caps hidden ">${symbolArray[i].eng[1]}</span>
+            <span class="shiftCaps  hidden">${symbolArray[i].eng[0]}</span>
+        </span>
+        <span class="rus hidden">
+        <span class="caseDown">${symbolArray[i].rus[0]}</span>
+        <span class="caseUp hidden">${symbolArray[i].rus[1]}</span>
+        <span class="caps hidden">${symbolArray[i].rus[1]}</span>
+        <span class="shiftCaps hidden">${symbolArray[i].rus[0]}</span>
+        </span>
+    `;
+    keyboardKey.className = `keyboardKey ${symbolArray[i].eventCode}`
+    bodyKeyboardHTML.append(keyboardKey);
+}
+
+
+// console.log(symbolArray[1].eventCode);
+
+let codeSum = '';
+
+// Переключение языка по вводу с клавиатуры
+// document.onkeydown = function (event) {
+//     if (event.key.charCodeAt(0) > 160) {
+//         console.log('рус');
+//     }
+
+// }
+
+
+// Нажатие клавишь
+
+document.onkeydown = function (event) {
+
+    console.log(event.key);
+
+
+
+    // Нажатие CapsLock
+
+    if (event.key == 'CapsLock') {
+
+        document.querySelectorAll('.caps').forEach(function (element) {
+            element.classList.toggle('hidden');
+        });
+        document.querySelectorAll('.caseDown').forEach(function (element) {
+            element.classList.toggle('hidden');
+        });
+        document.querySelectorAll('.caseUp,  .shiftCaps').forEach(function (element) {
+            element.classList.add('hidden');
+        });
+    };
+
+    // Нажатие Shift
+
+
+    if (event.key == 'Shift') {
+
+        document.querySelectorAll('.caps').forEach(function (element) {
+            if (element.classList.contains('hidden')) {
+
+
+                document.querySelectorAll('.caseUp').forEach(function (elem) {
+                    elem.classList.remove('hidden');
+                });
+                document.querySelectorAll('.caseDown, .caps, .shiftCaps').forEach(function (elem) {
+                    elem.classList.add('hidden');
+                });
+            };
+        });
+
+        document.querySelectorAll('.caps').forEach(function (element) {
+            if ((element.classList.contains('hidden')) == false) {
+
+                document.querySelectorAll('.shiftCaps').forEach(function (elem) {
+                    elem.classList.remove('hidden');
+                });
+                document.querySelectorAll('.caseDown, .caps, .caseUp').forEach(function (elem) {
+                    elem.classList.add('hidden');
+                });
+            };
+        });
+
+
+    };
+
+
+
+
+    //     if (event.key.charCodeAt(0) > 160) {
+    //         document.querySelectorAll('.rus').forEach(function (element) {
+    //             element.classList.remove('hidden');
+    //         });
+    //         document.querySelectorAll('.eng').forEach(function (element) {
+    //             element.classList.add('hidden');
+    //     });
+    // }
+    //     if ((event.key.charCodeAt(0) <= 160) && (event.key.charCodeAt(0) > 32)) {
+    //         document.querySelectorAll('.eng').forEach(function (element) {
+    //             element.classList.remove('hidden');
+    //         });
+    //         document.querySelectorAll('.rus').forEach(function (element) {
+    //             element.classList.add('hidden');
+    //     });
+    // }
+
+
+    console.log(event.key)
+    let keyDown = document.querySelector(`.${event.code}`);
+    keyDown.classList.add('active');
+    let code = '';
+
+let classKey = function() {
+    if ((keyDown.querySelector('.caseDown').classList.contains('hidden'))==false) {
+        return '.caseDown';
+    }
+    if ((keyDown.querySelector('.caseUp').classList.contains('hidden'))==false) {
+        return '.caseUp';
+    }
+    if ((keyDown.querySelector('.caps').classList.contains('hidden'))==false) {
+        return '.caps';
+    }
+    if ((keyDown.querySelector('.shiftCaps').classList.contains('hidden'))==false) {
+        return '.shiftCaps';
+    }
+}
+
+    code = keyDown.querySelector(`${classKey()}`).innerText;
+
+    // Присвоение спецсимволам значений
+    if (keyDown.classList.contains('Tab')) { code = '\t' };
+    if (keyDown.classList.contains('Enter')) { code = '\n' };
+    if (keyDown.classList.contains('CapsLock')) { code = '' };
+    if (keyDown.classList.contains('ShiftLeft')) { code = '' };
+    if (keyDown.classList.contains('ShiftRight')) { code = '' };
+    if (keyDown.classList.contains('ControlLeft')) { code = '' };
+    if (keyDown.classList.contains('ControlRight')) { code = '' };
+    if (keyDown.classList.contains('MetaLeft')) { code = '' };
+    if (keyDown.classList.contains('AltLeft')) { code = '' };
+    if (keyDown.classList.contains('Delete')) { code = '' };
+    if (keyDown.classList.contains('Backspace')) { code = '' };
+
+    codeSum += code;
+
+    // Backspace
+    if (keyDown.classList.contains('Backspace')) { codeSum = codeSum.substring(0, codeSum.length - 1) };
+
+    document.querySelector('.fieldText').value = `${codeSum}`;
+}
+
+document.onkeyup = function (event) {
+
+
+    // Отпускание Shift
+
+    if (event.key == 'Shift') {
+
+        document.querySelectorAll('.caps').forEach(function (element) {
+            if (element.classList.contains('hidden')) {
+
+                document.querySelectorAll('.caseDown').forEach(function (elem) {
+                    elem.classList.remove('hidden');
+                });
+
+                document.querySelectorAll('.caseUp').forEach(function (elem) {
+                    elem.classList.add('hidden');
+                });
+            };
+        });
+
+        document.querySelectorAll('.shiftCaps').forEach(function (element) {
+            if ((element.classList.contains('hidden')) == false) {
+
+                document.querySelectorAll('.shiftCaps, .caseDown, .caseUp').forEach(function (elem) {
+                    elem.classList.add('hidden');
+                });
+
+                document.querySelectorAll('.caps').forEach(function (elem) {
+                    elem.classList.remove('hidden');
+                });
+            };
+        });
+
+    };
+
+
+    document.querySelectorAll('.keyboardKey').forEach(function (element) {
+        element.classList.remove('active');
+    })
+};
+
+// Нажатие мышью
+
+document.querySelector('.CapsLock').onclick = function() {
+    document.querySelectorAll('.caps').forEach(function (element) {
+        element.classList.toggle('hidden');
+    });
+    document.querySelectorAll('.caseDown').forEach(function (element) {
+        element.classList.toggle('hidden');
+    });
+    document.querySelectorAll('.caseUp,  .shiftCaps').forEach(function (element) {
+        element.classList.add('hidden');
+    });
+};
+
+document.querySelectorAll('.keyboardKey').forEach(function (element) {
+    element.onmousedown = function () {
+
+
+
+        document.querySelectorAll('.keyboardKey').forEach(function (element) {
+            element.classList.remove('active');
+        });
+
+        this.classList.add('active');
+
+
+        let code = '';
+        code = this.querySelector('.caseDown').innerText;
+        if (this.classList.contains('Tab')) { code = '\t' };
+        if (this.classList.contains('Enter')) { code = '\n' };
+        if (this.classList.contains('CapsLock')) { code = '' };
+        if (this.classList.contains('ShiftLeft')) { code = '' };
+        if (this.classList.contains('ShiftRight')) { code = '' };
+        if (this.classList.contains('ControlLeft')) { code = '' };
+        if (this.classList.contains('ControlRight')) { code = '' };
+        if (this.classList.contains('MetaLeft')) { code = '' };
+        if (this.classList.contains('AltLeft')) { code = '' };
+        if (this.classList.contains('Delete')) { code = '' };
+        if (this.classList.contains('Backspace')) { code = '' };
+        codeSum += code;
+        if (this.classList.contains('Backspace')) { codeSum = codeSum.substring(0, codeSum.length - 1) };
+        console.log(codeSum);
+        document.querySelector('.fieldText').value = `${codeSum}`;
+    }
+ 
+    });
+
+
+    document.querySelectorAll('.keyboardKey').forEach(function (element) {
+        element.onmouseup = function () {
+    
+        
+            document.querySelectorAll('.keyboardKey').forEach(function (element) {
+                element.classList.remove('active');
+            });
+        }
+    });
+
